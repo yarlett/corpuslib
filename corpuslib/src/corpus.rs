ae616d6c0ec98a511f1a395de7e48b1aafd4bc74
Daniel Yarlett <daniel.yarlett@gmail.com>
2015-09-23 19:15:13 -0700

    Simple creation of Corpus from vector of strings.

--- corpuslib/src/corpus.rs
+++ corpuslib/src/corpus.rs
@@ -1,10 +1,26 @@
+use std::collections::HashMap;
+
 struct Corpus {
-    corpus: i32,
-    suffix: i32,
+    corpus: Vec<u64>,
+    suffix: Vec<u64>,
 }
 
 impl Corpus {
-    fn new(corpus: i32, suffix: i32) -> Corpus {
+    fn new(tokens: Vec<String>) -> Corpus {
+        // Allocate corpus and suffix.
+        let n = tokens.len();
+        let mut corpus = vec![0; n];
+        let mut suffix = vec![0; n];
+        // Assign integers to corpus.
+        let mut map = HashMap::new();
+        let mut i:u64 = 0;
+        for token in tokens.iter() {
+            match map.get(token) {
+                Some(&v) => { corpus.push(v) },
+                _ => { map.insert(token, i); corpus.push(i); i += 1}
+            }
+        }
+        // Return.
         Corpus { corpus: corpus, suffix: suffix }
     }
 }
