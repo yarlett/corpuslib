61708277da033f1240a3fa8a17a4fdd4d6559fba
Daniel Yarlett <daniel.yarlett@gmail.com>
2015-09-29 21:25:32 -0700

    Linear search for ngram in corpus.

--- corpuslib/src/corpus.rs
+++ corpuslib/src/corpus.rs
@@ -35,13 +35,26 @@ impl Corpus {
         // Return.
         Corpus { corpus: corpus, suffix: suffix, wordmap: wordmap }
     }
+
+    pub fn search_linear(&self, seq: Vec<usize>) -> Vec<usize> {
+        let mut pos: Vec<usize> = Vec::new();
+        for p in 0..self.corpus.len() {
+            if seq_ordering_n(&self.corpus[p..], &seq[..], seq.len()) == cmp::Ordering::Equal {
+                pos.push(p)
+            }
+        }
+        pos
+    }
 }
 
-pub fn seq_ordering(seq1: &[usize], seq2: &[usize]) -> cmp::Ordering {
+pub fn seq_ordering_n(seq1: &[usize], seq2: &[usize], n: usize) -> cmp::Ordering {
     let (n1, n2) = (seq1.len(), seq2.len());
-    let n = cmp::min(n1, n2);
+    let mut look_n = cmp::min(n1, n2);
+    if n < look_n {
+        look_n = n;
+    }
     // Make comparisons.
-    for pos in 0..n {
+    for pos in 0..look_n {
         if seq1[pos] < seq2[pos] {
             return cmp::Ordering::Less;
         }
@@ -49,16 +62,20 @@ pub fn seq_ordering(seq1: &[usize], seq2: &[usize]) -> cmp::Ordering {
             return cmp::Ordering::Greater;
         }
     }
-    // If comparisons fail to find a difference, go by length.
-    if n1 == n2 {
+    // All assigned elements are the same, so make comparison based on length (shorter is lesser).
+    if look_n == n {
         return cmp::Ordering::Equal;
-    }
-    else if n1 < n2 {
+    } else if n1 < n2 {
         return cmp::Ordering::Less;
     }
     else {
         return cmp::Ordering::Greater;
     }
+}
+
+pub fn seq_ordering(seq1: &[usize], seq2: &[usize]) -> cmp::Ordering {
+    let (n1, n2) = (seq1.len(), seq2.len());
+    seq_ordering_n(seq1, seq2, cmp::max(n1, n2) + 1)
 }
 
 #[cfg(test)]
@@ -68,9 +85,7 @@ mod tests {
     use std::cmp;
     use super::*;
 
-    #[test]
-    fn check_corpus() {
-        let (ntypes, ntokens) = (100, 10000);
+    fn random_corpus(ntypes: usize, ntokens: usize) -> Corpus {
         // Generate a corpus of strings.
         let mut tokens: Vec<String> = Vec::with_capacity(ntokens);
         for _ in 0..ntokens {
@@ -79,6 +94,14 @@ mod tests {
         }
         // Create the corpus.
         let c = Corpus::new(tokens);
+        c
+    }
+
+    #[test]
+    fn check_corpus() {
+        // Generate random corpus.
+        let (ntypes, ntokens) = (100, 10000);
+        let c = random_corpus(ntypes, ntokens);
         // Check corpus and suffix array are the same length.
         if c.corpus.len() != ntokens || c.suffix.len() != ntokens {
             assert!(false);
@@ -91,5 +114,17 @@ mod tests {
             println!("{:?}", ord);
             assert!(ord != cmp::Ordering::Greater);
         }
+    }
+
+    #[test]
+    fn check_search_linear() {
+        // Generate random corpus.
+        let (ntypes, ntokens) = (10, 10000);
+        let c = random_corpus(ntypes, ntokens);
+        //
+        let mut seq = Vec::new();
+        seq.extend(c.corpus[0..3].iter());
+        let cpos = c.search_linear(seq);
+        println!("{:?}", cpos);
     }
 }
